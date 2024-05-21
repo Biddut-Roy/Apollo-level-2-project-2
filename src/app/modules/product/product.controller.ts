d156db4b9cc2c27a9f4921d71b7491c3f742ccee
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-21 22:27:23 +0600

    product create api controlar complete

---
+++ src/app/modules/product/product.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { ProductService } from './product.service';
+
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const productData = req.body;
+    const result = await ProductService.createProductDB(productData);
+    res.status(200).json({
+      success: true,
+      message: 'Product created successfully!',
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+      error: err,
+    });
+  }
+};
+
+export const ProductControlars = {
+  createProduct,
+};
