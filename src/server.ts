2d369826f612e1d1e0fa7252ecfc0de0f789e29f
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-21 18:59:32 +0600

    connect a database my server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
-import app from "./App";
-const port = 5000;
+import mongoose from 'mongoose';
+import app from './App';
+import config from './app/config';
 
 async function main() {
   try {
-    // await mongoose.connect(config.database_url as string);
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
